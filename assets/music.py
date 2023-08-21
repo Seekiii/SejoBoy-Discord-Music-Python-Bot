6d0a978dd4e0dd61c1314721d07fafabf5ad6a06
šeki <seekii.official@gmail.com>
2023-08-21 22:52:07 +0200

    [ADD] Play command.

--- assets/music.py
+++ assets/music.py
@@ -1,1 +1,43 @@
+import youtube_dl
+import discord
+import datetime
+
 from assets.config import *
+
+async def convert_minutes(sec):
+    hour = sec // 3600
+    minutes = (sec % 3600) // 60
+    seconds = sec % 60
+    return hour, minutes, seconds
+
+async def create_embed_play(data):
+    duration = await convert_minutes(data['duration'])
+    if duration[0] == 0:
+        duration = f"{duration[1]:02d}:{duration[2]:02d}"
+    else:
+        duration = f"{duration[0]:02d}:{duration[1]:02d}:{duration[2]:02d}"
+    embed = discord.Embed(title='Song found!', description=f'```css\n{data["uploader"].replace(" - Topic","")} - {data["title"]}\n```',color=config.embed_color,timestamp=datetime.datetime.utcnow())
+    embed.add_field(name='Duration', value=duration)
+    embed.add_field(name='Ask', value=f"<@{data['ask']}>")
+    embed.add_field(name='Uploaded by', value=f'[{data["uploader"]}]({data["uploader_url"]})')
+    embed.add_field(name='URL', value=f'[Youtube]({data["url"]})')
+    embed.set_thumbnail(url=data['thumbnail'])
+
+    return [embed,f"{data['uploader'].replace(' - Topic','')} - {data['title']}"]
+
+async def search_youtube(query):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0'
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(query, download=False)
+        url = None
+        if 'entries' in info:
+            info = info['entries'][0]
+        else:
+            return None
+        return info
--- bot.py
+++ bot.py
@@ -13,5 +13,43 @@ bot = commands.Bot()
 async def on_ready():
     print(config.on_ready_msg)
 
+@bot.slash_command(name="play",description="Play a song in your voice channel.",guild_only=True)
+@discord.option("name",str,description="Search for a name or send a YouTube link.",required=True)
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def _play(ctx,name=None):
+    await ctx.defer()
+    msg = await ctx.respond("Please wait...")
+
+    #= ERRORS =#:
+    if not name:
+        return await msg.edit(content="You have not typed the name of the song you want to play.")
+
+    if not ctx.author.voice:
+        return await msg.edit(content="You are not in the voice channel. You need to be in a voice channel to use this command.")
+
+    bot_permissions = ctx.author.voice.channel.permissions_for(ctx.guild.get_member(bot.user.id))
+    if not bot_permissions.connect:
+        return await msg.edit(content=f"The bot doesn't have permission to join the voice channel <#{ctx.author.voice.channel.id}>.")
+
+    if not bot_permissions.speak:
+        return await msg.edit(content=f"The bot doesn't have permission to speak/talk in the voice channel <#{ctx.author.voice.channel.id}>.")
+
+    bot_voice = ctx.author.guild.voice_client
+    if bot_voice is None or not bot_voice.is_connected():
+        await ctx.author.voice.channel.connect()
+
+    song_info = await search_youtube(name+config.search_fix)
+    song_info['ask'] = ctx.author.id
+
+    bot_voice = ctx.author.guild.voice_client
+    if ctx.author.voice.channel.id != bot_voice.channel.id:
+        return await msg.edit(content=f"The bot is in the voice channel <#{voice_channel.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
+
+    embed = await create_embed_play(song_info)
+    embed, title = embed[0], embed[1]
+
+    bot_voice.play(discord.FFmpegPCMAudio(song_info['url'], options='-vn'))
+    return await msg.edit(content="",embed=embed)
+
 
 bot.run(config.dc_token)
