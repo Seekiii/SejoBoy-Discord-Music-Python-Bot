2c725828a934f97f692a26a5484799a3c6d1e1d4
šeki <seekii.official@gmail.com>
2023-08-22 13:42:02 +0200

    [ADD] Queue.

--- assets/music.py
+++ assets/music.py
@@ -10,13 +10,15 @@ async def convert_minutes(sec):
     seconds = sec % 60
     return hour, minutes, seconds
 
-async def create_embed_play(data):
+async def create_embed_play(data,queue=False):
     duration = await convert_minutes(data['duration'])
     if duration[0] == 0:
         duration = f"{duration[1]:02d}:{duration[2]:02d}"
     else:
         duration = f"{duration[0]:02d}:{duration[1]:02d}:{duration[2]:02d}"
-    embed = discord.Embed(title='Song found!', description=f'```css\n{data["uploader"].replace(" - Topic","")} - {data["title"]}\n```',color=config.embed_color,timestamp=datetime.datetime.utcnow())
+    embed = discord.Embed(title='Song found!', description=f'```css\n{data["uploader"].replace(" - Topic","")} - {data["title"]}\n```',color=config.embed_color['play'],timestamp=datetime.datetime.now())
+    if queue:
+        embed = discord.Embed(title='Song added to the queue!', description=f'```css\n{data["uploader"].replace(" - Topic","")} - {data["title"]}\n```',color=config.embed_color['queue'],timestamp=datetime.datetime.now())
     embed.add_field(name='Duration', value=duration)
     embed.add_field(name='Ask', value=f"<@{data['ask']}>")
     embed.add_field(name='Uploaded by', value=f'[{data["uploader"]}]({data["uploader_url"]})')
@@ -32,12 +34,14 @@ async def search_youtube(query):
         'default_search': 'auto',
         'source_address': '0.0.0.0'
     }
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(query, download=False)
-        url = None
-        if 'entries' in info:
-            info = info['entries'][0]
-        else:
-            return None
-        return info
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(query, download=False)
+            url = None
+            if 'entries' in info:
+                info = info['entries'][0]
+            else:
+                return None
+            return info
+    except:
+        return None
--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands, tasks
 
+import datetime
 # Assets
 from assets.config import *
 from assets.music import *
@@ -9,14 +10,30 @@ from assets.logs import *
 
 bot = commands.Bot()
 
+queue = []
+
 @bot.event
 async def on_ready():
     print(config.on_ready_msg)
+    queue_start.start()
+
+@tasks.loop(seconds = 5)
+async def queue_start():
+    global queue
+    if len(queue) > 1:
+        current = queue[0]
+        _next = queue[1]
+        if current['voice'].is_playing():
+            print("Još svira")
+        else:
+            current['voice'].play(discord.FFmpegPCMAudio(_next['url'], options='-vn'))
+            del queue[0]
 
 @bot.slash_command(name="play",description="Play a song in your voice channel.",guild_only=True)
 @discord.option("name",str,description="Search for a name or send a YouTube link.",required=True)
 @commands.cooldown(1, 3, commands.BucketType.user)
 async def _play(ctx,name=None):
+    global queue
     await ctx.defer()
     msg = await ctx.respond("Please wait...")
 
@@ -45,12 +62,16 @@ async def _play(ctx,name=None):
 
     bot_voice = ctx.author.guild.voice_client
     if not ctx.author.voice or not ctx.author.voice.channel or ctx.author.voice.channel.id != bot_voice.channel.id:
-        return await msg.edit(content=f"The bot is in the voice channel <#{voice_channel.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
+        return await msg.edit(content=f"The bot is in the voice channel <#{bot_voice.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
 
-    embed = await create_embed_play(song_info)
-    embed, title = embed[0], embed[1]
-
-    bot_voice.play(discord.FFmpegPCMAudio(song_info['url'], options='-vn'))
+    if len(queue) == 0:
+        embed = await create_embed_play(song_info)
+        embed, title = embed[0], embed[1]
+        bot_voice.play(discord.FFmpegPCMAudio(song_info['url'], options='-vn'))
+    else:
+        embed = await create_embed_play(song_info,queue=True)
+        embed, title = embed[0], embed[1]
+    queue.append({"title":title,"embed":embed,"url":song_info['url'],"voice":bot_voice})
     return await msg.edit(content="",embed=embed)
 
 @bot.slash_command(name="stop",description="Stop a song in your voice channel.",guild_only=True)
@@ -63,7 +84,7 @@ async def _stop(ctx):
         return await msg.edit(content=f"The bot is not connected to any voice channel.")
 
     if not ctx.author.voice or not ctx.author.voice.channel or ctx.author.voice.channel.id != bot_voice.channel.id:
-        return await msg.edit(content=f"The bot is in the voice channel <#{voice_channel.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
+        return await msg.edit(content=f"The bot is in the voice channel <#{bot_voice.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
 
     if bot_voice.is_playing():
         bot_voice.stop()
@@ -82,7 +103,7 @@ async def _pause(ctx):
         return await msg.edit(content=f"The bot is not connected to any voice channel.")
 
     if not ctx.author.voice or not ctx.author.voice.channel or ctx.author.voice.channel.id != bot_voice.channel.id:
-        return await msg.edit(content=f"The bot is in the voice channel <#{voice_channel.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
+        return await msg.edit(content=f"The bot is in the voice channel <#{bot_voice.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
 
     if bot_voice.is_playing():
         bot_voice.pause()
@@ -102,7 +123,7 @@ async def _resume(ctx):
         return await msg.edit(content=f"The bot is not connected to any voice channel.")
 
     if not ctx.author.voice or not ctx.author.voice.channel or ctx.author.voice.channel.id != bot_voice.channel.id:
-        return await msg.edit(content=f"The bot is in the voice channel <#{voice_channel.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
+        return await msg.edit(content=f"The bot is in the voice channel <#{bot_voice.channel.id}>. You need to be in the same voice channel as the bot to use that command.")
 
     if bot_voice.is_paused():
         bot_voice.resume()
@@ -111,6 +132,24 @@ async def _resume(ctx):
         await msg.edit(content=f"The bot has already resumed.")
     else:
         await msg.edit(content=f"The bot isn't playing any song, and because of that, it cannot be resumed.")
-        
+
+@bot.slash_command(name="queue",description="See the queue list of songs in your voice channel.",guild_only=True)
+async def _queue(ctx):
+    await ctx.defer()
+    msg = await ctx.respond("Please wait...")
+
+    queue_string = ""
+    if len(queue) > 0:
+        for num in range(len(queue)):
+            song = queue[num]
+            if num == 0:
+                queue_string += f"[ **NOW** ] - {song['title']}\n"
+            else:
+                queue_string += f"[ **{num}** ] - {song['title']}\n"
+        print(queue_string)
+        embed = discord.Embed(title='Queue', description=queue_string, color=config.embed_color['queue'], timestamp=datetime.datetime.now())
+        return await msg.edit(content="", embed=embed)
+    return await msg.edit(content="Queue is empty.")
+
 #from assets.config2 import *
 bot.run(config.dc_token)
